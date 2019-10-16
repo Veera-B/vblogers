39e8c3310f64e854b602c04f8cb144deb15d2bec
Veera B <veerabashipangu@gmail.com>
2019-10-16 14:47:06 +0530

    static prefixes

--- vblogers/urls.py
+++ vblogers/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('search/',include('search.urls')),
     path('carts/',include('carts.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
